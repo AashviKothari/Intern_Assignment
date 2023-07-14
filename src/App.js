740b5699ec9a9621532c3449dbb11a16cd3a7d99
AashviKothari <aashvikothari78@gmail.com>
2023-07-14 18:50:22 +0530

    result

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchPosts } from './actions/actions';
 import './App.css';
 
-function App() {
+const App = () => {
+  const dispatch = useDispatch();
+  const { posts, isLoading, error } = useSelector((state) => state);
+
+  useEffect(() => {
+    dispatch(fetchPosts());
+  }, [dispatch]);
+
+  if (isLoading) {
+    return <p className="loading">Loading...</p>;
+  }
+
+  if (error) {
+    return <p className="error">Error: {error}</p>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="title">Posts</h1>
+      {posts.map((post) => (
+        <div key={post.id} className="post">
+          <h2 className="post-title">{post.title}</h2>
+          <p className="post-body">{post.body}</p>
+        </div>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
