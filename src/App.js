6919c5d186309eb5acd4dbc3d8fded1d00a472d2
AashviKothari <aashvikothari78@gmail.com>
2023-07-15 15:44:35 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -5,24 +5,24 @@ import './App.css';
 import Try from './reducers/Try';
 
 const App = () => {
-  // const dispatch = useDispatch();
-  // const { users, isLoading, error } = useSelector((state) => state);
+  const dispatch = useDispatch();
+  const { users, isLoading, error } = useSelector((state) => state);
 
-  // useEffect(() => {
-  //   dispatch(fetchUsers());
-  // }, [dispatch]);
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, [dispatch]);
 
-  // if (isLoading) {
-  //   return <p className="loading">Loading...</p>;
-  // }
+  if (isLoading) {
+    return <p className="loading">Loading...</p>;
+  }
 
-  // if (error) {
-  //   return <p className="error">Error: {error}</p>;
-  // }
+  if (error) {
+    return <p className="error">Error: {error}</p>;
+  }
 
   return (
     <div className="container">
-      {/* <h1>Users</h1>
+      <h1>Users</h1>
       {users.map((user) => (
         <div key={user.login.uuid} className="user">
           <img src={user.picture.medium} alt={user.name.first} />
@@ -30,8 +30,8 @@ const App = () => {
           <p>Email: {user.email}</p>
           <p>Location: {user.location.city}, {user.location.country}</p>
         </div>
-      ))} */}
-      <Try/>
+      ))}
+      {/* <Try/> */}
     </div>
   );
 };
